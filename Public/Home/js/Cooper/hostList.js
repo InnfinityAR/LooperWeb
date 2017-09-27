ea535e22c2627431785162fd35a1eaa619ff88ef
Yinsr <sr.yin@innfinityar.com>
2017-09-27 16:19:57 +0800

    hostList编辑指向

--- Public/Home/js/Cooper/hostList.js
+++ Public/Home/js/Cooper/hostList.js
@@ -103,7 +103,7 @@ $(function () {
                             })
                         });
                         hostList = res.data;
-                        $(".edit").click(function () {
+                        $(".hostList  .edit").click(function () {
                             var id =  $(this).attr("data-index");
                             location.href = "/Home/Cooper/editHost/id="+id;
                         });
@@ -155,7 +155,7 @@ $(function () {
                 })
             });
             hostBrandList = res.data;
-            $(".edit").click(function () {
+            $("hostBrandList .edit").click(function () {
                 var id =  $(this).attr("data-index");
                 location.href = "/Home/Cooper/hostBrand/id="+id;
             });
